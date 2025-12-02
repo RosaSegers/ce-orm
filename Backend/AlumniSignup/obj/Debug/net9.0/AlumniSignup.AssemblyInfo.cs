59b43cf53a92380c026190eeb793b5c946da5397
Rosa <rosa.segers.2001@gmail.com>
2025-12-02 12:56:56 +0100

    test

--- Backend/AlumniSignup/obj/Debug/net9.0/AlumniSignup.AssemblyInfo.cs
+++ Backend/AlumniSignup/obj/Debug/net9.0/AlumniSignup.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlumniSignup")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+747050d9e3c623c5d48fc19ce5bc709372e0d92f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc94fb28f9590a1c76128f1a97941938fd640e53")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlumniSignup")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlumniSignup")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
